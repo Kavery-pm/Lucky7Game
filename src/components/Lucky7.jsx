799ea5e3bf09538c28dfd5cda16455d74757c18e
kavery-pm <shiny.kavery@gmail.com>
2023-06-12 16:34:22 +0200

    displayed a message you won when the sum of dice roll is 7

--- src/components/Lucky7.jsx
+++ src/components/Lucky7.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import { dice6, getRolls } from "../utils/utils";
+import { dice6, getRolls, sum } from "../utils/utils";
 import "./Lucky7.css";
 const Lucky7 = ()=>{
    const [dieRolls, setdieRolls] = useState(getRolls(2));
+   const won = sum(dieRolls) === 7 ;
    const rollDice = ()=>{
   let newDiceNumnbers=getRolls(2);
   setdieRolls(newDiceNumnbers)
+  console.log(sum(dieRolls))
    }
     return (
         <main className="Lucky7">
-        <h1>Lucky7 </h1>
+        <h1>Lucky7 {won && "You won"} </h1>
         <section className="Lucky7-dice">
           <div className="Lucky7-die">{dieRolls[0]}</div>
           <div className="Lucky7-die">{dieRolls[1]}</div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,11 @@
-const dice6 = ()=>{
-    return Math.floor((Math.random()*6)+1)
-}
-const getRolls = (n)=>{
-    
-return Array.from({length:n},()=>dice6());
-}
-export {getRolls,dice6}
+const sum = (arr) => {
+
+  return arr.reduce((prev, curr) => prev + curr, 0);
+};
+const dice6 = () => {
+  return Math.floor(Math.random() * 6 + 1);
+};
+const getRolls = (n) => {
+  return Array.from({ length: n }, () => dice6());
+};
+export { getRolls, dice6, sum };
