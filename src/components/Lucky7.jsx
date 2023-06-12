4b914d676fca588d97d578a1a76bc621f3af1010
kavery-pm <shiny.kavery@gmail.com>
2023-06-12 13:31:13 +0200

    added the die roll state and used the function from utils

--- src/components/Lucky7.jsx
+++ src/components/Lucky7.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { dice6, getRolls } from "../utils/utils";
 import "./Lucky7.css";
 const Lucky7 = ()=>{
-    console.log(getRolls(7));
+   const [dieRolls, setdieRolls] = useState(getRolls(2));
+   const rollDice = ()=>{
+  let newDiceNumnbers=getRolls(2);
+  setdieRolls(newDiceNumnbers)
+   }
     return (
         <main className="Lucky7">
         <h1>Lucky7 </h1>
         <section className="Lucky7-dice">
-          <div className="Lucky7-die">{1}</div>
-          <div className="Lucky7-die">{2}</div>
+          <div className="Lucky7-die">{dieRolls[0]}</div>
+          <div className="Lucky7-die">{dieRolls[1]}</div>
         </section>
-        <button >Roll Again!</button>
+        <button onClick={rollDice} >Roll Again!</button>
       </main>
     )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ const dice6 = ()=>{
     return Math.floor((Math.random()*6)+1)
 }
 const getRolls = (n)=>{
-    console.log(n)
+    
 return Array.from({length:n},()=>dice6());
 }
 export {getRolls,dice6}
